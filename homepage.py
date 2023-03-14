0b1fba5a0b8cb1a20411e33959dde0b4d270c5ed
Rayaan Syyed <2566444s@student.gla.ac.uk>
2023-03-14 16:49:24 +0000

    Update pages have been added

--- homepage.py
+++ homepage.py
@@ -41,7 +41,7 @@ def create_flash():
 def delete_flash(query):
     print(f'attempting to delete: "{query}" from db...')
     connect = sqlite3.connect('flashcard.db')
-    cursor = connect.execute("SELECT answer question FROM flashcard WHERE question=(?)", (query, ))
+    cursor = connect.execute("SELECT answer question FROM flashcard WHERE question=(?)", (query,))
     answer = cursor.fetchone()[0]
     print(f'found row: question = "{query}", answer = "{answer}"')
     cursor.execute("DELETE FROM flashcard WHERE question=(?) AND answer=(?)", (query, answer))
@@ -52,24 +52,49 @@ def delete_flash(query):
     return redirect('/')
 
 
+@home.route('/edit_question/', methods=['POST', 'GET'])
+def update_question_flash():
+    connect = sqlite3.connect('flashcard.db')
+    cursor = connect.cursor()
+    cursor.execute("""SELECT question FROM flashcard""")
+    results = cursor.fetchall()
+    print("These be the questions:")
+    print(results)
+    if request.method == "POST":
+        question = request.form.get("Question")
+        newquestion = request.form.get("NewQuestion")
+        print("oh, the question is ", question)
+        cursor.execute("UPDATE flashcard SET question = (?) WHERE question = (?)", (newquestion, question))
+        connect.commit()
+        connect.close()
+    else:
+        connect.commit()
+        connect.close()
+    return render_template("updatequestion.html", results=results)
+    # [
+
+
 @home.route('/update/', methods=['POST', 'GET'])
 def update_flash():
     connect = sqlite3.connect('flashcard.db')
     cursor = connect.cursor()
     cursor.execute("""SELECT question FROM flashcard""")
     results = cursor.fetchall()
+    print("These be the questions:")
     print(results)
     if request.method == "POST":
         question = request.form.get("Question")
         answer = request.form.get("Answer")
         flashcard = Flashcard(question, answer)
-        print("you are smelly")
+        print("Results after form results: ")
+        print(results)
         print("Question: " + flashcard.question)
         print("Answer: " + flashcard.answer)
         cursor.execute("UPDATE flashcard SET answer = (?) WHERE question = (?)", (flashcard.answer, flashcard.question))
         connect.commit()
         connect.close()
-
+    # [('Whats 9 + 10?q=', '21'), ('How can you perform decompression?', "I don't know")]
+    # [('How can you perform decompression?',), ('Whats 9 + 10?q=',)]
     else:
         connect.commit()
         connect.close()
