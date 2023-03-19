e24aa3a2bf093bbfc4f63e7a4abf8fca393f0204
Rayaan Syyed <2566444s@student.gla.ac.uk>
2023-03-19 16:00:46 +0000

    Refactored code and added navbar

--- Main.py
+++ Main.py
@@ -1,21 +1,29 @@
 # an object of WSGI application
-from flask import Flask
-
+from flask import Flask, session
 from flashrouters import flash
-from questions import question
 from homepage import home
-from userjoin import user
+from userjoin import user_join
+from database.database import Database
+
 app = Flask(__name__)  # Flask constructor
-app.register_blueprint(flash) #flashcard functionality: create, delete, update
-app.register_blueprint(question) #redundant code
-app.register_blueprint(home)
-app.register_blueprint(user)
+app.config['SECRET_KEY'] = 'sorrylol2'
 
 
+@app.before_request
+def before_request():
+    session.permanent = False
+    session.modified = True
 
+
+def main():
+    global app
+    app.register_blueprint(flash)  # flashcard functionality: create, delete, update
+    app.register_blueprint(home)
+    app.register_blueprint(user_join)
+    database = Database('flashcard.db')
+    database.db_initialise()
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
-
-
-    app.run(debug=True)
+    main()
